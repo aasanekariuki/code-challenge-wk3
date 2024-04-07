d1bb6c782d7ef93fbb83593f2303bee85769f9ab
aasane kariuki <franciskariki053@gmail.com>
2024-04-07 18:50:03 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,106 @@
+// Your code here
+
+const baseURL = `http://localhost:3000/films`
+
+fetch(baseURL, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+
+}).then((res) => res.json())
+
+  .then((data) => {
+
+    const movieTitles = document.getElementById('films')
+
+
+    data.forEach((title) => {
+        const paragraph = document.createElement('li')
+
+        paragraph.classList.add("film", "item")
+
+        paragraph.addEventListener("click", async () => {
+            currentFilmId = title.id;
+            const selectedFilm = await fetchFilmById(currentFilmId);
+            updateFilmDetails(selectedFilm);
+        });
+
+
+
+        paragraph.innerText = title.title
+
+        movieTitles.append(paragraph)}) 
+        
+    })
+
+    const fetchFilmById = async(id) => {
+
+        const response = await 
+
+        fetch(`${baseURL}/${id}`);
+
+        const filmData = await response.json();
+
+        return filmData;
+    }
+
+    const updateFilmDetails = (selectedFilm) => {
+        console.log(selectedFilm.poster);
+
+        const posters = document.getElementById('poster')
+
+        posters.src = selectedFilm.poster
+
+        const movieTitles = document.getElementById('title')
+
+        movieTitles.innerHTML = selectedFilm.title
+
+        const runTime = document.getElementById('runtime')
+
+        runTime.innerHTML = selectedFilm.runtime + 'Minutes'
+
+        const descriptions = document.getElementById('film-info')
+
+        descriptions.innerHTML = selectedFilm.description
+
+        const showtime = document.getElementById('showtime')
+
+        showtime.innerHTML = selectedFilm.showtime
+
+        const ticketsAvailable = selectedFilm.capacity - selectedFilm.tickets_sold  
+
+        document.getElementById('ticket-num').innerHTML = ticketsAvailable
+
+
+
+fetch(baseURL, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            tickets_sold: ticketsAvailable
+        })
+    
+    }).then((res) => {
+        if (res.ok){
+            console.log('ticket sold successful');
+        }
+        else{
+            console.error('failed ticket not bought');
+        }
+    })
+    .catch(error => {
+        console.error('error updating tickets sold', error)
+    })
+
+    .catch(error => {
+        console.error('error fetching films', error)
+    })
+
+}
+    
+    
+   
